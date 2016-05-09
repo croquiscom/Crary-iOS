fc82b4f31f03cfef738f0234b3ed24ff7759183a
Sangmin Yoon <sangmin.yoon@croquis.com>
2016-05-09 14:24:41 +0900

    remove swift support

--- Pod/Classes/Util/String+Crary.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension String {
-    public func stringByAppendingPathComponent(str: String) -> String {
-        return (self as NSString).stringByAppendingPathComponent(str)
-    }
-}
